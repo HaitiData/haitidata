b47fa24eca27712d2fdf5cbe0abb910f1ddc567c
Irwan Fathurrahman <meomancer@gmail.com>
2017-10-11 12:53:51 +0700

    Geonode architecture 2
    * update fixture region
    
    * update geoserver and geonode in one container
    
    * clean code
    
    * update prod setting to receive PROXY_ALLOWED_HOSTS
    
    * update architecture to be like geonode
    
    * update DATABASE setting
    
    * update DATABASE setting
    
    * update DATABASE setting
    
    * update DATABASE setting
    
    * update settings
    
    * clean code
    
    * update setting to access /geoserver without /api/geoserver
    
    * clean code
    
    * clean code
    
    * Use uwsgi rather than development server.
    
    * clean code
    
    * update compose and docker file of geoserver
    
    * update django_haitidata
    
    * update django_haitidata 2
    
    * update django_haitidata 3
    
    * update settings

--- deployment/production/docker/django_haitidata/build.sh
+++ deployment/production/docker/django_haitidata/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 IMAGE_NAME=haitidata_django
-TAG_NAME=latest
+TAG_NAME=1.0
 docker build -t dimasciput/${IMAGE_NAME} .
 docker tag dimasciput/${IMAGE_NAME}:latest dimasciput/${IMAGE_NAME}:${TAG_NAME}
 docker push dimasciput/${IMAGE_NAME}:${TAG_NAME}
