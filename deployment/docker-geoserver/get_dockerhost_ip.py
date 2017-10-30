e8d959965067dd840287647574ad967f53454542
dimasciput <dimas.ciputra@gmail.com>
2017-10-30 08:47:57 +0200

    Update geoserver configuration

--- deployment/docker-geoserver/get_dockerhost_ip.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import docker
-
-BOOTSTRAP_IMAGE_CHEIP = 'codenvy/che-ip:nightly'
-
-client = docker.from_env()
-print client.info()
-client.containers.run(BOOTSTRAP_IMAGE_CHEIP, network_mode='host')
