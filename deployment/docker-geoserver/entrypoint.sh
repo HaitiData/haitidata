ff425d28355c1d1e27dbfb278ba275318fb756f0
dimasciput <dimas.ciputra@gmail.com>
2017-11-03 14:02:10 +0200

    Revert entrypoint

--- deployment/docker-geoserver/entrypoint.sh
+++ deployment/docker-geoserver/entrypoint.sh
@@ -13,7 +13,7 @@ echo "Remove redundant library..."
 echo "Cleaning old cluster settings ..."
 rm -vf $CLUSTER_CONFIG_DIR/*
 
-# export GEOSERVER_OPTS='-Djava.awt.headless=true -server -Xms16G -Xmx32G -Xrs -XX:PerfDataSamplingInterval=500 -Dorg.geotools.referencing.forceXY=true -XX:SoftRefLRUPolicyMSPerMB=36000 -XX:+UseParallelGC -XX:NewRatio=2 -XX:+CMSClassUnloadingEnabled'
+export GEOSERVER_OPTS='-Djava.awt.headless=true -server -Xms16G -Xmx32G -Xrs -XX:PerfDataSamplingInterval=500 -Dorg.geotools.referencing.forceXY=true -XX:SoftRefLRUPolicyMSPerMB=36000 -XX:+UseParallelGC -XX:NewRatio=2 -XX:+CMSClassUnloadingEnabled'
 export CATALINA_OPTS='-DCLUSTER_CONFIG_DIR="$CLUSTER_CONFIG_DIR" -Dactivemq.base="$CLUSTER_CONFIG_DIR/tmp" -Dactivemq.transportConnectors.server.uri="tcp://0.0.0.0:0?maximumConnections=1000&wireFormat.maxFrameSize=104857600&jms.useAsyncSend=true&transport.daemon=true&trace=true"'
 export JAVA_OPTS=$GEOSERVER_OPTS
 
