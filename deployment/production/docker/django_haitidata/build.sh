b0de680382bc0b49e6877e8d55f7f4999691a68a
Dimas <dimas.ciputra@gmail.com>
2017-12-04 16:36:27 +0700

    Force clean build docker image

--- deployment/production/docker/django_haitidata/build.sh
+++ deployment/production/docker/django_haitidata/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 IMAGE_NAME=haitidata_django
 TAG_NAME=latest
-docker build -t kartoza/${IMAGE_NAME} .
+docker build --no-cache -t kartoza/${IMAGE_NAME} .
 docker tag kartoza/${IMAGE_NAME}:latest kartoza/${IMAGE_NAME}:${TAG_NAME}
 docker push kartoza/${IMAGE_NAME}:${TAG_NAME}
